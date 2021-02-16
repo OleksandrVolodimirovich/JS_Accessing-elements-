28b2cacf0573b3af072a67368a6962448f5a3feb
OleksandrVolodimirovich <61995509+OleksandrVolodimirovich@users.noreply.github.com>
2021-02-16 13:21:48 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,111 @@
+let openBtn = document.getElementById('open-btn');
+let name = document.querySelector('.name');
+let nameValue = document.querySelector('.name-value');
+let budget = document.querySelector('.budget');
+let budgetValue = document.querySelector('.budget-value');
+let goods = document.querySelector('.goods');
+let goodsValue = document.querySelector('.goods-value');
+let items = document.querySelector('.items');
+let itemsValue = document.querySelector('.items-value');
+let employers = document.querySelector('.employers');
+let employersValue = document.querySelector('.employers-value');
+let discount = document.querySelector('.discount');
+let discountValue = document.querySelector('.discount-value');
+let isopen = document.querySelector('.isopen');
+let isopenValue = document.querySelector('.isopen-value');
+let goodsItem = document.querySelectorAll('.goods-item');
+let btn = document.getElementsByTagName('button');
+let chooseItem = document.querySelector('.choose-item');
+let timeValue = document.querySelector('.time-value');
+let countBudgetValue = document.querySelector('.count-budget-value');
+let hireEmployersItem = document.querySelectorAll('.hire-employers-item');
+
+//style shop
+
+let div = document.getElementsByTagName('div');
+div[2].style.backgroundColor = '#d6baba';
+div[3].style.borderRadius = '20px';
+div[3].style.borderColor = '#418994';
+div[3].style.color = '#6d7475';
+div[4].style.borderRadius = '20px';
+div[4].style.borderColor = '#418994';
+div[4].style.color = '#6d7475';
+div[5].style.borderRadius = '20px';
+div[5].style.borderColor = '#418994';
+div[5].style.color = '#6d7475';
+div[6].style.borderRadius = '20px';
+div[6].style.borderColor = '#418994';
+div[6].style.color = '#6d7475';
+div[7].style.borderRadius = '20px';
+div[7].style.borderColor = '#418994';
+div[7].style.color = '#6d7475';
+div[8].style.borderRadius = '20px';
+div[8].style.borderColor = '#418994';
+div[8].style.color = '#6d7475';
+div[9].style.borderRadius = '20px';
+div[9].style.borderColor = '#418994';
+div[9].style.color = '#6d7475';
+div[10].style.borderRadius = '20px';
+div[10].style.borderColor = '#418994';
+div[10].style.color = '#6d7475';
+div[11].style.borderRadius = '20px';
+div[11].style.borderColor = '#418994';
+div[11].style.color = '#6d7475';
+div[12].style.borderRadius = '20px';
+div[12].style.borderColor = '#418994';
+div[12].style.color = '#6d7475';
+div[13].style.borderRadius = '20px';
+div[13].style.borderColor = '#418994';
+div[13].style.color = '#6d7475';
+div[14].style.borderRadius = '20px';
+div[14].style.borderColor = '#418994';
+div[14].style.color = '#6d7475';
+div[15].style.borderRadius = '20px';
+div[15].style.borderColor = '#418994';
+div[15].style.color = '#6d7475';
+div[16].style.borderRadius = '20px';
+div[16].style.borderColor = '#418994';
+div[16].style.color = '#6d7475';
+
+btn[1].style.padding = '4px';
+btn[1].style.borderStyle = 'double';
+btn[1].style.color = '#06103b';
+btn[1].style.backgroundColor = '#acb8e3';
+btn[1].style.borderRadius = '5px';
+btn[2].style.padding = '4px';
+btn[2].style.borderStyle = 'double';
+btn[2].style.color = '#06103b';
+btn[2].style.backgroundColor = '#acb8e3';
+btn[2].style.borderRadius = '5px';
+btn[3].style.padding = '4px';
+btn[3].style.borderStyle = 'double';
+btn[3].style.color = '#06103b';
+btn[3].style.backgroundColor = '#acb8e3';
+btn[3].style.borderRadius = '5px';
+
+console.log(btn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
